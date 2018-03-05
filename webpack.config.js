d27d566cf8e93cd4d6e0bcf74a4ef453c3981cd4
rewel garcia <rgjr22@gmail.com>
2018-03-05 11:33:24 -0800

    Create webpack file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
