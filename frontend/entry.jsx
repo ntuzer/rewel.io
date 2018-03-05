0d285f5c9058be9565189a5f9f57cec226280f63
rewel garcia <rgjr22@gmail.com>
2018-03-05 11:26:09 -0800

    Create entry file

---
+++ frontend/entry.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('entry file');
+  const root = document.getElementById('root');
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+
+    // Clean up after ourselves so we don't accidentally use the
+    // global currentUser instead of the one in the store
+    delete window.currentUser;
+
+  } else {
+    store = configureStore();
+  }
+  // window.store = store;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.removeBookmark = removeBookmark;
+  // console.log('does it come back?');
+  ReactDOM.render(<Root store={store} />, root);
+});
