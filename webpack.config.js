ddf93bed51b6f37f2083245b0c649f9ac6e5a8fd
rewel garcia <rgjr22@gmail.com>
2018-03-20 15:53:11 -0700

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,82 @@
+//
+// var path = require("path");
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/entry.jsx",
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['env', 'react']
+//         }
+//       },
+//       {
+//         // The important stuff
+//         test: /\.(jpg|jpeg|png|ico|gif)(\?.*)?$/, // Load only .jpg .jpeg, and .png files
+//         use: {
+//           loader: 'file-loader',
+//           options: {
+//             name: '[name][md5:hash].[ext]', // Name of bundled asset
+//             outputPath: '/images/', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
+//             publicPath: '/assets/' // Endpoint asset can be found at on Rails server
+//           }
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   }
+// };
 
 var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
   context: __dirname,
   entry: "./frontend/entry.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
-    rules: [
+    loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['env', 'react']
+          presets: ['react', 'env']
         }
       },
       {
