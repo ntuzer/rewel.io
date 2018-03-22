dedc58d92fa8b3bdccd3a8212703c351920ded5f
rewel garcia <rgjr22@gmail.com>
2018-03-22 15:13:31 -0700

    Change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,66 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/entry.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        // The important stuff
+        test: /\.(jpg|jpeg|png|ico|gif)(\?.*)?$/, // Load only .jpg .jpeg, and .png files
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name][md5:hash].[ext]', // Name of bundled asset
+            outputPath: '/images/', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
+            publicPath: '/assets/' // Endpoint asset can be found at on Rails server
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
+
+// webpack.config.js
 // var path = require("path");
 // var webpack = require("webpack");
 //
@@ -59,54 +122,3 @@
 //     extensions: [".js", ".jsx", "*"]
 //   }
 // };
-
-// webpack.config.js
-var path = require("path");
-var webpack = require("webpack");
-
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
-
-// include plugins config
-module.exports = {
-  context: __dirname,
-  entry: "./frontend/entry.jsx",
-  output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
-  },
-  plugins: plugins,
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env']
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
-};
